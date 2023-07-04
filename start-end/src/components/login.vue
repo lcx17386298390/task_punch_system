3d49e6fc33710c45085d096335a9f51e67d26ac4
羊驼想睡觉 <204532647@qq.com>
2023-07-04 18:47:31 +0800

    修改了登录页面

--- start-end/src/components/login.vue
+++ start-end/src/components/login.vue
@@ -1,61 +1,103 @@
 <template>
   <div class="login-container">
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <label for="username">Username</label>
-      <input type="text" id="username" v-model="username">
-      
-      <label for="password">Password</label>
-      <input type="password" id="password" v-model="password">
-      
-      <button type="submit">Login</button>
-    </form>
+    <div class="card-wrapper">
+      <div class="card-inner user-login" :class="{ 'selected': selectedLogin === 'user' }" @mouseover="selectUserLogin">
+        <h2>普通登录</h2>
+        <!-- 普通登录表单 -->
+        <form>
+          <!-- 添加其他登录字段 -->
+          <input type="text" placeholder="用户名" />
+          <input type="password" placeholder="密码" />
+          <button type="submit">登录</button>
+        </form>
+      </div>
+      <div class="card-inner admin-login" :class="{ 'selected': selectedLogin === 'admin' }" @mouseover="selectAdminLogin">
+        <h2>管理员登录</h2>
+        <!-- 管理员登录表单 -->
+        <form>
+          <!-- 添加其他登录字段 -->
+          <input type="text" placeholder="管理员用户名" />
+          <input type="password" placeholder="管理员密码" />
+          <button type="submit">登录</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name:'login',
   data() {
     return {
-      username: '',
-      password: ''
+      selectedLogin: "", // 默认未选择任何登录方式
     };
   },
   methods: {
-    login() {
-      // 在这里执行登录逻辑，可以向服务器发送登录请求或进行其他处理
-      console.log('登录成功！');
-    }
-  }
-}
+    selectUserLogin() {
+      this.selectedLogin = "user";
+    },
+    selectAdminLogin() {
+      this.selectedLogin = "admin";
+    },
+  },
+};
 </script>
 
 <style>
 .login-container {
-  max-width: 300px;
-  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 
-h1 {
+.card-wrapper {
+  position: relative;
+  perspective: 1000px;
+  cursor: pointer;
+}
+
+.card-inner {
+  width: 300px;
+  height: 400px;
+  position: absolute;
+  padding: 20px;
+  background-color: #f2f2f2;
+  transition: transform 0.3s, box-shadow 0.3s;
+  backface-visibility: hidden;
+}
+
+.admin-login {
+  background-color: #ddd;
+}
+
+.card-wrapper .selected {
+  transform: translate(0, -50%) scale(1.1);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  z-index: 1;
+}
+
+h2 {
   text-align: center;
   margin-bottom: 20px;
 }
 
-label {
-  display: block;
-  margin-top: 10px;
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 input {
-  width: 100%;
-  padding: 5px;
-  margin-top: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
 }
 
 button {
-  width: 100%;
   padding: 10px;
-  margin-top: 10px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  cursor: pointer;
 }
 </style>
