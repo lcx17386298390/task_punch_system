714672b02bec88f39be0d0fd9f78e48f69b4e425
Violet <3104044743@qq.com>
2023-07-04 20:18:25 +0800

    加邮箱

---
+++ back-end/taskManageSys-msgToMail/src/main/java/com/acm/msgtomail/front/controller/msgToMailController.java
@@ -0,0 +1,9 @@
+package com.acm.msgtomail.front.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin
+@RestController
+public class msgToMailController {
+
+}
