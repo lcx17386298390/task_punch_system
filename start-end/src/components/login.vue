611ac71306812f3953b1f406294569bcc7405156
羊驼想睡觉 <204532647@qq.com>
2023-07-04 19:20:11 +0800

    修改了登录的样式

--- start-end/src/components/login.vue
+++ start-end/src/components/login.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="login-container">
-    <div class="card-wrapper">
-      <div class="card-inner user-login" :class="{ 'selected': selectedLogin === 'user' }" @mouseover="selectUserLogin">
-        <h2>普通登录</h2>
-        <!-- 普通登录表单 -->
-        <form>
-          <!-- 添加其他登录字段 -->
-          <input type="text" placeholder="用户名" />
-          <input type="password" placeholder="密码" />
-          <button type="submit">登录</button>
-        </form>
-      </div>
-      <div class="card-inner admin-login" :class="{ 'selected': selectedLogin === 'admin' }" @mouseover="selectAdminLogin">
-        <h2>管理员登录</h2>
-        <!-- 管理员登录表单 -->
-        <form>
-          <!-- 添加其他登录字段 -->
-          <input type="text" placeholder="管理员用户名" />
-          <input type="password" placeholder="管理员密码" />
-          <button type="submit">登录</button>
-        </form>
-      </div>
+    <div
+      class="card-inner user-login"
+      :class="{ 'selected': selectedLogin === 'user' }"
+      @click="selectLogin('user')"
+    >
+      <h2>普通登录</h2>
+      <form>
+        <input type="text" placeholder="用户名" />
+        <input type="password" placeholder="密码" />
+        <button type="submit">登录</button>
+      </form>
+    </div>
+    <div
+      class="card-inner admin-login"
+      :class="{ 'selected': selectedLogin === 'admin' }"
+      @click="selectLogin('admin')"
+    >
+      <h2>管理员登录</h2>
+      <form>
+        <input type="text" placeholder="管理员用户名" />
+        <input type="password" placeholder="管理员密码" />
+        <button type="submit">登录</button>
+      </form>
     </div>
   </div>
 </template>
@@ -29,17 +31,14 @@
 export default {
   data() {
     return {
-      selectedLogin: "", // 默认未选择任何登录方式
+      selectedLogin: "user"
     };
   },
   methods: {
-    selectUserLogin() {
-      this.selectedLogin = "user";
-    },
-    selectAdminLogin() {
-      this.selectedLogin = "admin";
-    },
-  },
+    selectLogin(loginType) {
+      this.selectedLogin = loginType;
+    }
+  }
 };
 </script>
 
@@ -48,33 +47,19 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-}
-
-.card-wrapper {
-  position: relative;
-  perspective: 1000px;
-  cursor: pointer;
 }
 
 .card-inner {
-  width: 300px;
+  width: 350px;
   height: 400px;
-  position: absolute;
   padding: 20px;
   background-color: #f2f2f2;
-  transition: transform 0.3s, box-shadow 0.3s;
-  backface-visibility: hidden;
+  transition: background-color 0.5s;
 }
 
-.admin-login {
-  background-color: #ddd;
-}
-
-.card-wrapper .selected {
-  transform: translate(0, -50%) scale(1.1);
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-  z-index: 1;
+.card-inner.selected {
+  background-color: #007bff;
+  color: #fff;
 }
 
 h2 {
