b8eed5e64e9580cb2c27145fd50098385aa7a60f
Kansan1 <2641344238@qq.com>
2023-07-06 20:51:16 +0800

    站内消息

--- back-end/taskManageSys-msgToMail/src/main/java/com/acm/msgtomail/front/controller/msgToMailController.java
+++ back-end/taskManageSys-msgToMail/src/main/java/com/acm/msgtomail/front/controller/msgToMailController.java
@@ -1,9 +1,30 @@
 package com.acm.msgtomail.front.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class msgToMailController {
+public class msgToMailController extends BaseController{
+
+    @RequestMapping("/sendmessage")
+    public String sendmessage(@RequestParam("fromuser") String fromuser,
+                              @RequestParam("touser") String touser,
+                              @RequestParam("msg") String msg)
+    {
+
+        int result=sendMsgService.insertMsgBytouser(fromuser,touser,msg);
+        if(result!=0)
+        {
+            return "发送成功！";
+        }else {
+            return "发送失败！";
+        }
+    }
+
+
+
 
 }
