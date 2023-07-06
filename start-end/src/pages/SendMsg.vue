6c01683f015385b0fc142cb799e85059dcc670d3
Winging__ <1173603136@qq.com>
2023-07-06 19:28:14 +0800

    ceshi

--- start-end/src/pages/SendMsg.vue
+++ start-end/src/pages/SendMsg.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="send_msg">
-    <textarea class="text" cols="50" rows="10"></textarea>
-    <button class="btn">send</button>
+    <div class="checkbox">
+      <label v-for="person in people" :key="person.id">
+      <input type="checkbox" :value="person.id" v-model="selectedPeople"> {{ person.name }}
+    </label>
+    </div>
+    <textarea v-model="message" class="text" cols="50" rows="10"></textarea>
+    <button  @click="sendMsg" class="btn">send</button>
     </div>
 </template>
 
@@ -10,9 +15,35 @@ export default {
   neme:'SendMsg',
   data(){
     return{
-        text:''
+        text:'',
+        people: [
+        { id: 1, name: '张三二' },
+        { id: 2, name: '李四颐' },
+        { id: 3, name: '王五' },
+        { id: 4, name: '李四' },
+        { id: 5, name: '反倒是' },
+        { id: 6, name: '附件是' },
+        { id: 7, name: '十分恼' },
+        { id: 8, name: '返回' }
+      ],
+      selectedPeople:[],
+      message: ''
     }
   },
+  methods:{
+    sendMsg() {
+      const selectedPeople = this.selectedPeople;
+      const message = this.message;
+
+      // axios.post('/api/send_msg', { selectedPeople, message })
+      //   .then(response => {
+      //     console.log('消息已发送');
+      //   })
+      //   .catch(error => {
+      //     console.error('发送消息时出错', error);
+      //   });
+  }
+  }
   
 }
 </script>
@@ -32,5 +63,10 @@ export default {
 .btn {
     margin-block-start: 10px;
 }
-
+.checkbox{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  margin-block-end: 10px;
+}
 </style>
