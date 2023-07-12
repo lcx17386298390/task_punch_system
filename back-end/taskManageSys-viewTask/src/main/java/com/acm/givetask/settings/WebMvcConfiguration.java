b04557b714ed234c45ac5c9657acebacb2550cf1
Violet <3104044743@qq.com>
2023-07-12 16:45:36 +0800

    chh-修bug

--- back-end/taskManageSys-viewTask/src/main/java/com/acm/givetask/settings/WebMvcConfiguration.java
+++ back-end/taskManageSys-viewTask/src/main/java/com/acm/givetask/settings/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.acm.front.settings;
+package com.acm.givetask.settings;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
