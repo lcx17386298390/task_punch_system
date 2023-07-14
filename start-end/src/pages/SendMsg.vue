5712c482745b043ec42ee4382facd68d67c4b83b
Winging__ <1173603136@qq.com>
2023-07-14 08:43:09 +0800

    改为发送接收信息两个模块

--- start-end/src/pages/SendMsg.vue
+++ start-end/src/pages/SendMsg.vue
@@ -18,8 +18,9 @@
     </div>
 
     <!-- 呈现消息 -->
-    <div class="content">
-      
+    <div class="content1">
+      <h3>发送信息</h3>
+      <hr>
       <ul>
         <li v-for="msg in sentMessages" :key="msg.id">
           <span>{{ msg.fromuser }}</span> 发了一个消息给 <span>{{ msg.touser }}</span> :
@@ -30,7 +31,19 @@
       </ul>
       
     </div>
-    
+    <div class="content2">
+      <h3>接收信息</h3>
+      <hr>
+      <ul>
+        <li v-for="msg in sentMessages" :key="msg.id">
+          <span>{{ msg.touser }}</span> 发了一个消息给 <span>{{ msg.fromuser }}</span> :
+          <p>{{ msg.msg }}</p>
+          {{currentDate}}
+          <hr>
+        </li>
+      </ul>
+      
+    </div>
     </div>
 </template>
 
@@ -84,8 +97,8 @@ export default {
           //   content: this.message
           // });
           if (item.isSelected) {
-          doGet("http://localhost:8002/tms/sendmessage", {
-            fromuser: 'You',
+          doGet("http://localh ost:8002/tms/sendmessage", {
+            fromuser: '你',
             toname: item.name,
             msg:this.message,
             id:Date.now()
@@ -140,7 +153,7 @@ export default {
   place-items: center;
   align-items: center;
   flex-direction: column;
-  margin: 10px 0 0 200px;
+  margin: 10px 0 0 150px;
 }
 .text {
   font-size: 20px;
@@ -173,17 +186,41 @@ export default {
   font-weight:bold;
 
 }
-.content {
+.content1 {
   position: absolute;
-  inline-size: 800px;
+  inline-size: 400px;
   block-size: 530px;
   border-radius: 20px;
   box-shadow: 0 0 30px #9cadb3a9;
-  margin: 0 0 0 1300px;
+  margin: 0 0 0 860px;
   overflow: auto;
   overflow-wrap: break-word; 
   line-height: 1.5;
   z-index: 2;
+}
+.content1 h3 {
+  margin: 20px 0 0 20px;
+  color: #181a1ebf;
+  font-size: 20px;
+  font-weight:bold;
+}
+.content2 {
+  position: absolute;
+  inline-size: 400px;
+  block-size: 530px;
+  border-radius: 20px;
+  box-shadow: 0 0 30px #9cadb3a9;
+  margin: 0 0 0 1720px;
+  overflow: auto;
+  overflow-wrap: break-word; 
+  line-height: 1.5;
+  z-index: 2;
+}
+.content2 h3 {
+  margin: 20px 0 0 20px;
+  color: #181a1ebf;
+  font-size: 20px;
+  font-weight:bold;
 }
 .content ul li {
   margin: 30px 30px 0 30px;
